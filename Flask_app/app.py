c50bd19ae1a4c747c306ffbc030c167914e7a140
BhargavArunkumar <77136030+BhargavArunkumar@users.noreply.github.com>
2021-08-03 08:18:22 +0530

    Add files via upload

---
+++ Flask_app/app.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template, request
+import joblib
+
+app = Flask(__name__)
+
+# load model
+model = joblib.load('dib_79.pkl')
+
+@app.route('/')
+
+def hello():
+    return render_template('predict.html')
+
+@app.route('/home')
+
+def home():
+    return render_template('index.html')
+
+@app.route('/contact')
+
+def contact():
+    return 'Contact'
+
+
+@app.route('/submit', methods = ['POST'])
+def form_data():
+    preg = request.form.get('preg')
+    plas = request.form.get('plas')
+    pres = request.form.get('pres')
+    skin = request.form.get('skin')
+    test = request.form.get('test')
+    mass = request.form.get('mass')
+    pedi = request.form.get('pedi')
+    age = request.form.get('age')
+    
+
+    output = model.predict([[preg,plas,pres,skin,test,mass,pedi,age]])
+
+    print(output)
+
+
+    if output[0] == 1:
+        out = 'diabatic'
+    else:
+        out = 'not diabatic'
+
+  
+
+    return render_template('display.html' , data = f"Person is {out}")
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
